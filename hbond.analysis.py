fbc07f70d7305b6ee265a8ba2d0c0ce4d74dc73a
Kelly Du Pont <dupontke@enterprise.chm.colostate.edu>
2016-07-26 17:55:54 -0600

    [updated hydrogen bond analysis with extracting time, distance and angle from MDAnalysis]

--- hbond.analysis.py
+++ hbond.analysis.py
@@ -69,9 +69,8 @@ out_list = []
 h_list = []
 table_list = []
 for i in range(nSel):
-    h = MDAnalysis.analysis.hbonds.HydrogenBondAnalysis(u,selection1 = sel[i][1],selection2 = sel[i][2],selection1_type='both',detect_hydrogens='distance',start=None,stop=2,step=None, distance=3.0, angle=120.0)
+    h = MDAnalysis.analysis.hbonds.HydrogenBondAnalysis(u, selection1 = sel[i][1], selection2 = sel[i][2], selection1_type='both', update_selection1=False, update_selection2=False, detect_hydrogens='distance', start=None, stop=None, step=None, distance=3.0, angle=120.0, donors=None, acceptors=None)
     h_list.append(h)
-
 
 # BEGINNING TO ANALYZE TRAJECTORIES
 nSteps = 0
@@ -79,39 +78,47 @@ while start_traj <= end_traj:
     ffprint('Loading trajectory %s' %(start_traj))
     u.load_new('%sproduction.%s/production.%s.dcd' %(traj_loc,start_traj,start_traj))
     nSteps += len(u.trajectory)
+##    t0 = u.trajectory.start_timestep / u.trajectory.skip_timestep * u.trajectory.dt
+##    t = [t0 + (ts.frame-1) * u.trajectory.dt for ts in u.trajectory]
+    t0 = u.trajectory.start_timestep / u.trajectory.skip_timestep * 0.002
+    t = [t0 + (ts.frame-1) * 0.002 for ts in u.trajectory]
+#    print t[:6]
+#
+##    frame_numbers = [ts.frame for ts in u.trajectory]
+##    print frame_numbers[:]
+#    sys.exit()
     for i in range(nSel):
+        out1 = open('%s.%s.results.dat' %(system,sel[i][0]), 'a')
         h_list[i].run()
-        htimeseries = h_list[i].timeseries
-        
-        print h_list[i].timesteps
-        htype = h_list[i].timesteps_by_type()
-        print htype
-        sys.exit()
-	print len(htimeseries)
+        htimeseries = h_list[i].timeseries     
+#        print htimeseries
+#        print len(h.timeseries)
+#        print h_list[i].timesteps
+#        bonded_hs = h_list[i]._get_bonded_hydrogens()
+#        print bonded_hs
+##       htype = h_list[i].timesteps_by_type()
+##        print htype
+#        sys.exit()
+#	print len(htimeseries)
 	for j in range(len(u.trajectory)):
-#		print htimeseries[0] 		### the h-bond data for frame i... 
-		for k in range(len(htimeseries[i])):
-#			print htimeseries[0][0]
-			for l in range(len(htimeseries[j][k])):
-				print htimeseries[0][0][-2], htimeseries[0][0][-1]
-				sys.exit()
-
-#        hgentable = h_list[i].generate_table()
-#        table = h_list[i].table
-#        table_list.append(table)
-#        out1 = open('%s.%s.table.dat' %(system,sel[i][0]), 'a')
-#        out1.write('%s' %(table))
-#        out1.close()
-
-        # extract number of frames and cooresponding hydrogen bond distances for the trajectories
-#        out2 = open('' %(system,))
-#        frames = len(htimeseries)
-#        ffprint('Frames = %s\n' %(frames))
+#        for j in range(1000):
+#            print 'frame',j
+##		print htimeseries[0] 		### the h-bond data for frame i... 
+            if len(h_list[i].timeseries[j]) == 0:
+                out1.write('%.6f    %.9f    %.9f\n' %(t[j],0.0,0.0))
+#                print t[j], 0.00, 0.00
+            else: 
+                for k in range(len(htimeseries[j])):
+##			print htimeseries[0][0]
+                #for l in range(len(htimeseries[j][k])):
+#                    print t[j], htimeseries[j][k][-2], htimeseries[j][k][-1]
+                    out1.write('%.6f    %.9f    %.9f\n' %(t[j],htimeseries[j][k][-2],htimeseries[j][k][-1]))
+        out1.close()
         
-	ffprint('Finished analyzing trajectory %02d\n' %(start_traj))
+#	ffprint('Finished analyzing trajectory %02d\n' %(start_traj))
     start_traj += 1
 
-ffprint('Analyzed %d steps.' %(nSteps))
+#ffprint('Analyzed %d steps.' %(nSteps))
 
-summary(nSteps)
+#summary(nSteps)
 
