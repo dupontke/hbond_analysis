ad2b37c84f58e41c4358a35dd7f44f9e4bda3269
Kelly Du Pont <dupontke@enterprise.chm.colostate.edu>
2016-07-26 09:45:22 -0600

    [a list of residue selections for hbond analysis]

---
+++ sel_list.py
@@ -0,0 +1,10 @@
+# NOTATION:
+# sel.append([descriptor, selection1, selection2])
+
+sel = []
+sel.append(['protein_test','protein','resid 77'])
+#sel.append(['S49R77_hbond','resid 49','resid 77'])
+#sel.append(['R50E104_hbond','resid 50','resid 104'])
+#sel.append(['R50E28_hbond','resid 50','resid 28'])
+#sel.append(['R50D139_hbond','resid 50','resid 139'])
+#sel.append(['R50D142_hbond','resid 50','resid 142'])
