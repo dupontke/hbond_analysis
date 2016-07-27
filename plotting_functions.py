03936c1e18fb36688e14cfcacd403c277786d62d
Kelly Du Pont <dupontke@enterprise.chm.colostate.edu>
2016-07-27 15:23:27 -0600

    [plotting functions for hbond analysis]

--- plotting_functions.py
+++ plotting_functions.py
@@ -8,6 +8,8 @@ import numpy as np
 import sys
 import os
 import matplotlib.pyplot as plt
+import matplotlib.colors as mcolors
+import matplotlib as mpl
 from matplotlib.ticker import NullFormatter
 
 stdev = np.std
@@ -16,6 +18,23 @@ nullfmt = NullFormatter()
 
 # ----------------------------------------
 # PLOTTING SUBROUTINES
+
+def make_colormap(seq):
+	"""Return a LinearSegmentedColormap
+	seq: a sequence of floats and RGB-tuples. The floats should be increasing
+	and in the interval (0,1).
+	"""
+	seq = [(None,) * 3, 0.0] + list(seq) + [1.0, (None,) * 3]
+	cdict = {'red': [], 'green': [], 'blue': []}
+	for i, item in enumerate(seq):
+		if isinstance(item, float):
+			r1, g1, b1 = seq[i - 1]
+			r2, g2, b2 = seq[i + 1]
+			cdict['red'].append([item, r1, r2])
+			cdict['green'].append([item, g1, g2])
+			cdict['blue'].append([item, b1, b2])
+	return mcolors.LinearSegmentedColormap('CustomMap', cdict)
+
 
 def plot_1d(xdata, ydata, color, x_axis, y_axis, system, analysis, average = False, t0 = 0, **kwargs):
 	""" Creates a 1D scatter/line plot:
@@ -35,6 +54,7 @@ def plot_1d(xdata, ydata, color, x_axis, y_axis, system, analysis, average = Fal
 		xunits, yunits: string with correct math text describing the units for the x/y data
 		x_lim, y_lim: list w/ two elements, setting the limits of the x/y ranges of plot
 		plt_title: string to be added as the plot title
+		draw_line: int value that determines the line style to be drawn; giving myself space to add more line styles if I decide I need them
 
 	"""
 	# INITIATING THE PLOT...
@@ -54,6 +74,12 @@ def plot_1d(xdata, ydata, color, x_axis, y_axis, system, analysis, average = Fal
 			plt.ylim(value)
 		elif name == 'plt_title':
 			plt.title(r'%s' %(value), size='14')
+		elif name == 'draw_line':
+			draw_line = value
+			if draw_line == 1:
+				plt.plot([0,max(ydata)],[0,max(ydata)],'r-',linewidth=2)
+			else:
+				print 'draw_line = %s has not been defined in plotting functions script' %(line_value)
 	
 	plt.grid(b=True, which='major', axis='both', color='#808080', linestyle='--')
 	plt.xlabel(r'%s' %(x_axis), size=12)
@@ -216,6 +242,49 @@ def scat_hist(xdata, ydata, color, x_axis, y_axis, system, analysis, num_b = 100
 	plt.close()
 
 
+def bar(xdata, ydata, x_axis, y_axis, system, analysis, **kwargs): 
+	""" Creates a bar graph
+	
+	Usage: bar(xdata, ydata, x_axis, y_axis, **kwarg)
+	
+	Arguments:
+	xdata, ydata: self-explanatory
+	x_axis, y_axis: strings to be printed on the axi labels
+	system: descriptor for the system analyzed
+	analysis: descriptor for the analysis performed and plotted
+	
+	kwargs:
+	xunits, yunits: string with correct math text describing the units for the x/y data
+	x_lim, y_lim: list (or tuple) w/ two elements, setting the limits of the x/y ranges of plot
+	plt_title: string to be added as the plot title
+	"""
+	
+	# INITIATING THE PLOT...
+	plt.bar(xdata,ydata)
+
+	# READING IN KWARG DICTIONARY INTO SPECIFIC VARIABLES
+	for name, value in kwargs.items():
+		if name == 'xunits':
+			x_units = value
+			x_axis = '%s (%s)' %(x_axis, value)
+		elif name == 'yunits':
+			y_units = value
+			y_axis = '%s (%s)' %(y_axis, value)
+		elif name == 'x_lim':
+			plt.xlim(value)
+		elif name == 'y_lim':
+			plt.ylim(value)
+		elif name == 'plt_title':
+			plt.title(r'%s' %(value), size='16')
+	
+	plt.grid(b=True, which='major', axis='both', color='#808080', linestyle='--')
+	plt.ylabel(r'%s' %(y_axis),size=12)
+	plt.xlabel(r'%s' %(x_axis),size=12)
+
+	plt.savefig('%s.%s.bar.png' %(system,analysis),dpi=300)
+	plt.close()
+
+
 def hist2d(xdata, ydata, x_axis, y_axis, num_b, system, analysis, norm):
 	""" Creates a 2D histogram (heat map)
 	
@@ -250,3 +319,56 @@ def hist2d(xdata, ydata, x_axis, y_axis, num_b, system, analysis, norm):
 	yedges = []
 	image = []
 
+
+def matrix2d(matrix, x_axis, y_axis, cb_axis, system, analysis, **kwargs):
+	""" Creates a 2D matrix image
+	
+	Usage: matrix2d(matrix,x_axis,y_axis,system,analysis)
+	
+	Arguments:
+	matrix: the data matrix to be plotted (should have shape of MxN, but can have MxNx3 or MxNx4)
+	x_axis, y_axis: strings to be printed on the axi labels
+	system: descriptor for the system analyzed
+	analysis: descriptor for the analysis performed and plotted
+        
+	kwargs:
+	vmin, vmax: floats that define the limits for the color bar; if below vmin, data will be colored white; if above vmax, data will be colored red (might want to change this for aesthetics)
+	plt_title: string to be added as the plot title
+	cb_units: sting to be added to the color bar label to indicate the units of the color bar 
+	"""
+
+	vmin =0.001
+	vmax = None
+
+	#c = mcolors.ColorConverter().to_rgb
+	#bgr = make_colormap([c('blue'),c('lime'),0.50,c('lime'),c('red'),1.00,c('red')])
+	#bgr = make_colormap([c('red'),c('lime'),0.50,c('lime'),c('blue'),1.00,c('blue')])
+	#bgr.set_under('k')
+	#bgr.set_over('r')
+	#bgr.set_over('w')
+	#my_cmap = bgr
+
+	my_cmap = plt.cm.get_cmap('jet')
+
+	#my_cmap = plt.cm.get_cmap('gray')
+
+	# READING IN KWARG DICTIONARY INTO SPECIFIC VARIABLES
+	for name, value in kwargs.items():
+		if name == 'vmin':
+			vmin = value
+		elif name == 'vmax':
+			vmax = value
+		elif name == 'cb_units':
+			cb_units = value
+			cb_axis = '%s (%s)' %(cb_axis, value)
+		elif name == 'plt_title':
+			plt.title(r'%s' %(value), size='14')
+	
+	plt.imshow(matrix,cmap=my_cmap,vmin=vmin,vmax=vmax,interpolation='none',origin='lower')
+	cb1 = plt.colorbar(extend='max',cmap=my_cmap)
+	cb1.set_label(r'%s' %(cb_axis), size=12)
+	plt.grid(b=True, which='major', axis='both', color='#808080', linestyle='--')
+	plt.xlabel(r'%s' %(x_axis), size=12)
+	plt.ylabel(r'%s' %(y_axis), size=12)
+	plt.savefig('%s.%s.matrix2d.png' %(system, analysis),dpi=300)
+	plt.close()
