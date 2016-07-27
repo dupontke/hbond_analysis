f1e71befb3fc0cf403ddb7410af1f49daf5c17c7
Kelly Du Pont <dupontke@enterprise.chm.colostate.edu>
2016-07-27 15:21:54 -0600

    [plotting hbond distances vs. time for specific residue pairs]

--- hbond_plotting.py
+++ hbond_plotting.py
@@ -1,6 +1,8 @@
 #!/Library/Frameworks/Python.framework/Versions/2.7/bin/python
 # USAGE:
 
+# ./hbond_plotting.py data_file system_descriptor
+
 # PREAMBLE:
 
 from plotting_functions import *
@@ -14,20 +16,15 @@ nSel = len(sel)
 # ----------------------------------------
 # MAIN PROGRAM:
 
+# Load in .dat_file
 datalist = np.loadtxt(dat)
+hbonds = len(datalist[:])
+print 'Number of selections: %d, Total hydrogen bonds found: %d' %(nSel,hbonds)
 
-nSteps = len(datalist[:])               # for 2D use the following: nSteps = len(datalist[:,0]             
-print 'Number of selections: %d, number of steps: %d' %(nSel,nSteps)
 
-time = np.zeros(nSteps)
-
-for i in range(nSteps):
-	time[i] = i*0.002		# units of time in ns; each frame is separated by 0.002 ns 
-
+# Run the plotting function for Hydrogen Bond Analysis.
+# Descriptions for scat_hist(xdata, ydata, color, x_axis, y_axis, system, analysis, **kwargs)
+# NOTE: [:,0] refers to [row,column]. [:] will loop through all the rows. [0] loop through the first column in all the rows. 
 for i in range(nSel):
 	selection = sel[i][0]
-
-	scat_hist(time[:],datalist[:],'k','Time (ns)','RMSD','%02d.%s' %(i,selection),'%s' %(system),yunits='$\AA$')
-
-
-# for the last for loop datalist[:,i]
+	scat_hist(datalist[:,0],datalist[:,1],'k','Time (ns)','Hydrogen Bond Distance','%02d.%s' %(i,selection),'%s' %(system),yunits='$\AA$',x_lim=(0,40))
