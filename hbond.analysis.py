4eafa186816d66f6536c557c7bf0cd766c87bf8a
Kelly Du Pont <dupontke@enterprise.chm.colostate.edu>
2016-07-28 15:12:31 -0600

    [updated hbond analysis]

--- hbond.analysis.py
+++ hbond.analysis.py
@@ -52,7 +52,7 @@ def summary(nSteps):
     sum_file.write('\nTotal number of steps analyzed: %d\n' %(nSteps))
     sum_file.write('\nAtom Selections analyzed:n')
     for i in range(nSel):
-        sum_file.write('%02d  %s %s\n' %(i,sel[i][0],sel[i][1],sel[i][2]))
+        sum_file.write('%02d  %s %s\n' %(i,sel[i][1],sel[i][2],sel[i][3]))
     sum_file.close()
 
 # ----------------------------------------
@@ -62,12 +62,14 @@ def summary(nSteps):
 # INITIALIZING UNIVERSE
 
 u = MDAnalysis.Universe(pdb)
-out_list = []
 h_list = []
 table_list = []
 for i in range(nSel):
-    h = MDAnalysis.analysis.hbonds.HydrogenBondAnalysis(u, selection1 = sel[i][1], selection2 = sel[i][2], selection1_type='both', update_selection1=False, update_selection2=False, detect_hydrogens='distance', start=None, stop=None, step=None, distance=3.0, angle=120.0, donors=None, acceptors=None)
+    if sel[i][0] != sel[i-1][0] or i == 0:
+        os.mkdir('%s' %(sel[i][0]))
+    h = MDAnalysis.analysis.hbonds.HydrogenBondAnalysis(u, selection1=sel[i][2], selection2=sel[i][3], selection1_type='donor', update_selection1=False, update_selection2=False, detect_hydrogens='distance', start=None, stop=None, step=None, distance=3.0, angle=120.0, donors={'NE'}, acceptors=None)
     h_list.append(h)
+
 
 # BEGINNING TO ANALYZE TRAJECTORIES
 nSteps = 0
@@ -80,19 +82,26 @@ while start_traj <= end_traj:
     t0 = nSteps * 0.002
     t = [t0 + (ts.frame-1) * 0.002 for ts in u.trajectory]
     nSteps += len(u.trajectory)
+
 # Loop through all residue pair selections, calculate the Hydrogen Bond distances and angles for all pairs
-# Write time (ns), distance, and angle into an output file named: system.sel[i][0].results.dat
+# Write time (ns), distance, and angle into an output file named: system.sel[i][1].results.dat
     for i in range(nSel):
-        out1 = open('%s.%s.results.dat' %(system,sel[i][0]), 'a')
+        os.chdir('%s' %(sel[i][0]))
+        out1 = open('%s.%s.results.dat' %(system,sel[i][1]), 'a')
         h_list[i].run()
         htimeseries = h_list[i].timeseries     
-	for j in range(len(u.trajectory)):
+	h_list[i].generate_table()
+        htable = h_list[i].table
+        for j in range(len(u.trajectory)):
             if len(h_list[i].timeseries[j]) != 0:
                 for k in range(len(htimeseries[j])):
                     out1.write('%.6f    %.9f    %.9f\n' %(t[j],htimeseries[j][k][-2],htimeseries[j][k][-1]))
         out1.close()
-        
-	ffprint('Finished analyzing trajectory %02d\n' %(start_traj))
+        out2 = open('%s.%s.table.dat' %(system,sel[i][1]), 'a')
+        out2.write('%s' %(htable))
+        out2.close()
+       	ffprint('Finished analyzing trajectory %02d\n' %(start_traj))
+        os.chdir('..')
     start_traj += 1
 
 ffprint('Analyzed %d steps.' %(nSteps))
